a96c07b2ae6ce7c93b2ef9e476f2491c51fbe7ff
Daniel Ariaz-Avalos <dannyariaz45@gmail.com>
2026-05-25 21:06:06 -0700

    init files

---
+++ biquad-filter/biquad.h
@@ -0,0 +1,10 @@
+//
+// Created by Daniel Ariaz-Avalos on 5/25/26.
+// A single biquad filter using Direct Form II Transposed (DF2T).
+// Implementation of a peak (bell) filter using the RBJ Audio EQ Cookbook formulas.
+//
+
+#ifndef BIQUAD_H
+#define BIQUAD_H
+
+#endif //BIQUAD_H
---
+++ biquad-filter/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Daniel Ariaz-Avalos on 5/25/26.
+//
+#include <iostream>
+#include "biquad.h"
+
+
+int main() {
+    std::cout << "Building a biquad peak filter..." << std::endl;
+    return 0;
+}
